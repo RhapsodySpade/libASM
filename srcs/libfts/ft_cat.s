b7413725d63c4b2f6c31b446f88ee86a6635ef5a
Mélodie Sagodira <melodie.sagodira@gmail.com>
2016-01-13 13:14:47 +0100

    fixed cat segfault

--- srcs/libfts/ft_cat.s
+++ srcs/libfts/ft_cat.s
@@ -1,38 +1,42 @@
 section .data
-	buffer times 2048 db 0
-	lenbuff equ $ - buffer
+    buffer times 2048 db 0
+    bufflen equ $ - buffer
 
 section .text
-	global _ft_cat
+    global _ft_cat
 
 _ft_cat:
-	cmp rdi, -1
-	jle out
 
-loop:
-	mov rax, 0x2000003 ;read() 
-	lea rsi, [rel buffer]
-	mov rdx, lenbuff
-	syscall
-	
-	cmp rax, 0
-	jle out
-	
-	push rdi
-	
-	mov rdx, rax
-	mov rax, 0x2000004
-	mov rdi, 1
-	lea rsi, [rel buffer]
-	syscall
-	
-	pop rdi
-	cmp rax, -1
-	je out
-	cmp rdi, 0
-	je loop
+while:
+
+    mov rax, 0x2000003
+    mov rsi, buffer
+    mov rdx, bufflen
+    syscall
+    jc err
+
+    push rdi
+
+    cmp rax, 0
+    je out
+
+    mov rdx, rax
+    mov rax, 0x2000004
+    mov rdi, 1
+    mov rsi, buffer
+    syscall
+
+    pop rdi
+
+    jc err
+
+    jmp while
 
 out:
-	pop rdi
-	mov rax, 0
-	ret
+    pop rdi
+    mov rax, 0
+    ret
+
+err:
+    mov rax, 1
+    ret
